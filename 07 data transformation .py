51ed59af2286130edfa922ab9fa15b3579932139
Akash kolhe <87632720+Akashklh@users.noreply.github.com>
2021-09-21 17:45:17 +0530

    Add files via upload
    07

---
+++ 07 data transformation .py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 19 12:49:16 2021
+
+@author: Akash
+"""
+
+#-------------------------Reading & Writing data in Files----------------------
+
+import pandas
+
+# Reading CSV Files with Pandas:
+df = pandas.read_csv('C:/Users/91988/Desktop/python Library/files/User_Data.csv')
+print(df)
+
+# Writing CSV Files with Pandas:
+df.to_csv('IIT-B.csv')
+
+# Reading Excel Files with Pandas
+df1 = pandas.read_excel('C:/Users/91988/Desktop/python Library/files/User_Data.xlsx')
+
+df1 = pandas.read_excel('User_Data.xlsx')
+print(df1)
+
+# Writing Excel Files with Pandas 
+df1.to_excel('IIT-B.xlsx')
+df2 = pandas.DataFrame(df1)
+print (df2)
